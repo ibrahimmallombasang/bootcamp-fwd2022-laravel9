25e617f017d6e15a92e7dbfd9d92387166e32a88
ibrahimmallombasang <ibrahimmallombasang@github.com>
2022-06-21 23:40:36 +0800

    penambahan file migration table

---
+++ database/migrations/2022_06_16_131631_create_type_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('type_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); //default: required
+            $table->timestamps(); //created_at && update_at
+            $table->softDeletes(); //data tidak dapat di view tapi masih ada di db (field: deleted_at)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('type_user');
+    }
+};
